752f132e4ce165db854fcb46156d3a12969ae613
hk3380 <lp752@york.ac.uk>
2014-04-15 23:55:25 +0100

    almost a fully fledged concurrent todo list

--- erlang/todolist.erl
+++ erlang/todolist.erl
@@ -1,11 +1,19 @@
+% Concurrent TODO list
+
 -module(todolist).
 -compile(export_all).
 
 todo(List) ->
+    % The process receives signals in a direct asymmetric way
     receive
+        % Patterns to match and respond asynchronously
+
+        % Add item to the todo list
         {ID, add, Item} ->
             ID ! "added.",
             todo([Item|List]);
+
+        % Remove item
         {ID, remove, Item} ->
             case lists:member(Item, List) of
                 true ->
@@ -15,9 +23,18 @@ todo(List) ->
                     ID ! "not found.",
                     todo(List)
             end;
+
+        % Get entire list
+        {ID, list} ->
+            ID ! List,
+            todo(List);
+
     terminate ->
         ok
+
     end.
+
+% Some sorts of interface
 
 add(ID, Item) ->
     ID ! {self(), add, Item}.
@@ -25,5 +42,11 @@ add(ID, Item) ->
 remove(ID, Item) ->
     ID ! {self(), remove, Item}.
 
+list(ID) ->
+    ID ! {self(), list}.
+
+
+% Spawns a todo process that listens for messages to pattern match
+% Function returns ID, as the communication method is direct asymmetric naming
 start() ->
     spawn(?MODULE, todo, [[]]).
